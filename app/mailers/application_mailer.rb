e2e6768da7f09fa99725df75190959a25a1965da
namrataukirde <namrata.ukirde@kiprosh.com>
2017-03-05 02:27:29 +0530

    revert mistakenly added changes

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: 'namrata.ukirde@kiprosh.com'
+  default from: 'from@example.com''
   layout 'mailer'
 end
--- app/mailers/feedback_token_mailer.rb
+++ app/mailers/feedback_token_mailer.rb
@@ -2,7 +2,7 @@ class FeedbackTokenMailer < ApplicationMailer
   def feedback_token_email(person, feedback_token)
     @person = person
     @feedback_token = feedback_token
-    mail(to: "namrataukirde@gmail.com", subject: 'Feedback verification uniq token') do |format|
+    mail(to: person.email, subject: 'Feedback verification uniq token') do |format|
       format.text { render plain: feedback_token }
     end
   end
