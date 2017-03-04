a8faef58157720eeb88a4dc57ed4f970d2decaf6
namrataukirde <namrata.ukirde@kiprosh.com>
2017-03-04 23:41:28 +0530

    added nested attributes

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,4 +2,7 @@ class Question < ApplicationRecord
   belongs_to :topic
   has_many :answers
   as_enum :question_type, %i(rating subjective), prefix: true
+
+  accepts_nested_attributes_for :answers
+
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -1,4 +1,6 @@
 class Topic < ApplicationRecord
   has_many :questions
   belongs_to :person, class_name: 'CoreBox::Person'
+
+  accepts_nested_attributes_for :questions
 end
