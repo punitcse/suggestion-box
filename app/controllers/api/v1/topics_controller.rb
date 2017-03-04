c7d8b72ffa85eb01bb0efee08e9d51444bd32355
Punit Jain <punit.jain@kiprosh.com>
2017-03-05 03:19:26 +0530

    add admin UI for topic

--- app/controllers/api/v1/topics_controller.rb
+++ app/controllers/api/v1/topics_controller.rb
@@ -5,15 +5,6 @@ module Api::V1
     before_action :authenticate!, except: [:submit_feedback]
     before_action :find_topic, only: [:submit_feedback]
 
-    def create
-      @topic = Topic.new(topic_params)
-      if @topic.save
-        render json: @topic.to_json
-      else
-        render json: @topic.errors.to_json, status: 422
-      end
-    end
-
     def submit_feedback
       if @topic.update(feedback_params)
         head :ok
@@ -31,10 +22,6 @@ module Api::V1
 
     def find_topic
       @topic ||= Topic.find(params[:id])
-    end
-
-    def topic_params
-      params.require(:topic).permit(:name, :rating_scale, :description, :expiry_date)
     end
 
     def feedback_params
