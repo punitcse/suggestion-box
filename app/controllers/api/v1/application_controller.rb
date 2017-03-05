a8380944b33d2dbe32641a752195f9aa3f5f1f85
namrataukirde <namrata.ukirde@kiprosh.com>
2017-03-05 06:33:53 +0530

    Basic form to submit open suggestion

--- app/controllers/api/v1/application_controller.rb
+++
@@ -1,8 +0,0 @@
-module Api::V1
-  class ApplicationController < ::ActionController::API
-    protect_from_forgery
-    respond_to :json
-
-    include CoreBox::Authentication
-  end
-end
--- app/controllers/api/v1/suggestions_controller.rb
+++ app/controllers/api/v1/suggestions_controller.rb
@@ -1,13 +1,19 @@
 module Api::V1
   class SuggestionsController < ApplicationController
+    include CoreBox::Authentication
+
     before_action :authenticate!
 
+    def new
+      @suggestion = Suggestion.new
+    end
+
     def create
-      suggestion = Suggestion.new(suggestions_params)
+      @suggestion = Suggestion.new(suggestions_params)
       if suggestion.save
-        render json: suggestion
+        render json: @suggestion
       else
-        render json: suggestion.errors.full_messages, status: 422
+        render json: @suggestion.errors.full_messages, status: 422
       end
     end
 
